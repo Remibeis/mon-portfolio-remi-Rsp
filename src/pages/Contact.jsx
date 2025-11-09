85e77929cc2c02bd584115e7aa53b232704928c8
Remibeis <remibeis12@gmail.com>
2025-11-09 08:28:30 +0100

    update cv

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ return (
     <p> Email : remibeis12@gmail.com</p>
     <p> Téléphone : 06.15.80.16.58</p>
     <p> GitHub : <a href="https://github.com/Remibeis" target="_blank" rel="noopener noreferrer">github.com/Remibeis</a></p>
-    <a href="/Rémicv8.pdf" download className="cv-button">Télécharger mon CV</a>
+    <a href="/About Me (7).pdf" download className="cv-button">Télécharger mon CV</a>
     </div>
 );
 }
